9cbcc77affbe04e2bb6d355078057866bfacb869
fgorkhs <50909802+fgorkhs@users.noreply.github.com>
2020-01-09 17:42:40 +0800

    Add files via upload

--- login_example/main.php
+++ login_example/main.php
@@ -20,19 +20,6 @@
         <li><a data-toggle="tab" href="#knowledge" id="nav_knowledge" style="font-size:17px;font-weight:bold">水果小學堂🎓</a></li>
         <li><a data-toggle="tab" href="#pickfruit" id="nav_pickfruit" style="font-size:17px;font-weight:bold">水果偵探🔎</a></li>
         <li><a data-toggle="tab" href="#layerlist" id="nav_layerlist" style="font-size:17px;font-weight:bold">蔬果價格查詢📈</a></li>
-        <!-- <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">功能選單<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                <li><a href="#" id="fun1">功能1</a></li>
-                <li><a href="#" id="fun2">功能2</a></li>
-            </ul>
-        </li> -->
-      </ul>
-      <!-- <ul class="nav navbar-nav navbar-right">
-        <li><a id="membership" href="?member"><span class="glyphicon glyphicon-user"></span>會員資料</a></li>
-        <li><a id="logout" href="?signout"><span class="glyphicon glyphicon-log-out"></span>登出</a></li>
-      </ul> -->
-
       <ul class="nav navbar-nav navbar-right">
         <li><a id="membership" href="?member" style="font-weight:bold"><span class="glyphicon glyphicon-user"></span>會員資料</a></li>
         <li><a id="logout" href="?signout" style="font-weight:bold"><span class="glyphicon glyphicon-log-out"></span>登出</a></li>
@@ -84,20 +71,8 @@
 </nav>
 
 <div id="container" style='height: 100%;'>
-  <div id="sidebar"> <!-- style="display:none" -->
+  <div id="sidebar">
      <div id="accordion">
-
-
-
-<!--        <h3>水果介紹<button type="button" id="btn-hide" class="btn btn-xs btn-default pull-right" id="sidebar-hide-btn"><i class="fa fa-chevron-left"></i></button></h3>
-            <div id="acc_layerlist">
-              <h4 id='fName'></h4>
-              <div id="fImg">
-                  <img id="fruitImg" style="width:100%">
-              </div>
-              <div id="content"></div>
-            </div> -->
-
             <h3>Intro<button type="button" id="btn-hide" class="btn btn-xs btn-default pull-right" id="sidebar-hide-btn"><i class="fa fa-chevron-left"></i></button></h3>
             <div id="acc_layerlist1">
               <div style="margin-top:20px;">「果來一點好嗎！」是一個結合地圖跟臺灣水果相關資訊的網站。</div>
@@ -115,23 +90,6 @@
               <h3>Past five years:</h3>
               <div id="overtime"></div>
             </div>
-
-            <h3>圖層列表</h3>
-            <div id="acc_layerlist3">
-              <h4 id="baselayerlist">基本底圖</h4>
-              <h4 id="overlayerlist">套疊圖資</h4>
-            </div>
-            
-            <!-- <h3>繪圖</h3>
-            <div id="draw_data">
-              <button class='btn btn-info btn-draw' id='btn-line' drawType='Point'>Point</button>
-              <button class='btn btn-info btn-draw' id='btn-line' drawType='LineString'>LineString</button>
-              <button class='btn btn-info btn-draw' id='btn-line' drawType='Polygon'>Polygon</button>
-              <br />
-              <button class='btn btn-warning' id='btn-edit'>Edit</button><br />
-              <a href='javascript:void(0);' class='btn btn-info' id='btn-json'>Download GeoJSON</a>
-            </div> -->
-
     </div>
   </div>
   <div id="map"></div>
@@ -139,7 +97,5 @@
 <script src="https://d3js.org/d3.v4.js"></script>
 <script src='./js/monthlygraf.js'></script>
 <script src="./js/fruitDoc.js"></script>
-<!-- <script src="./js/countyBtn.js"></script> -->
-<script src="./js/map.js"></script>         <!-- include map.js here because it must appear after <div id="map"> -->
+<script src="./js/map.js"></script>
 <script src="./js/main.js"></script>
-<script src="./js/draw.js"></script>
