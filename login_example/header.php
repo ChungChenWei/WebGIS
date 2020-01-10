1f988622e9f7d3a1454d3672f9085e12408eff81
yenpingL <m10809211@gapps.ntust.edu.tw>
2020-01-10 14:39:03 +0800

    version 0.7 (期末報告版本)
    sign up 頁面改成中文
    修正css引入路徑

--- login_example/header.php
+++ login_example/header.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/bar_style.css">
-    <link rel="stylesheet" href="./css/style.css" type="text/css">
+    <link rel="stylesheet" href="../../css/bar_style.css">
+    <link rel="stylesheet" href="../../css/style.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
     <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
